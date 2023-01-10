33fdd0bf99104f7d6e39d5c7a322928e6bf9aeea
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2023-01-10 15:29:20 -0300

    "pdf pronto, falta interface"

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import webbrowser
+
+from fpdf import FPDF
 class Bill:
     def __init__(self, amount, period):
         self.amount = amount
@@ -10,9 +13,9 @@ class Flatmate:
         self.name =name
         self.days_in_house =days_in_house
 
-    def pays(self, bill, flatmate2):
-        weigth= self.days_in_house/(self.days_in_house+
-                                     flatmate2.days_in_house)
+    def pays(self, bill, flatemate2):
+        weigth= self.days_in_house/(self.days_in_house +
+                                    flatemate2.days_in_house)
         to_pay = bill.amount*weigth
         return to_pay
 
@@ -24,12 +27,32 @@ class PdfReport:
     def __init__(self, filename):
         self.filename =filename
 
-    def generate(selfself,flatemate1,flatmate2, bill):
-        pass
+    def generate(self,flatemate1,flatemate2, bill):
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+        pdf.image("house.jpg", w=30,h=30)
+
+        # add some text
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=88, txt="Flatemates Bill", border=1, align='C', ln=1)
+        pdf.cell(w=150, h=40, txt="Period", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=150, h=40, txt= flatemate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(round(flatemate1.pays(bill, flatemate2))), border=1, ln=1)
+        pdf.cell(w=150, h=40, txt=flatemate2.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(round(flatemate2.pays(bill,flatemate1))), border=1)
+        pdf.output(self.filename)
+
+        webbrowser.open(self.filename)
+
+
 #bill = Bill(120)
 #print(bill.amount)
 the_bill =Bill(amount=120, period="March 2021")
 john =Flatmate(name="John", days_in_house=20)
 marry=Flatmate(name="Marry", days_in_house=25)
-print("John pays",john.pays(bill=the_bill,flatmate2=marry))
-print("Marry pays",marry.pays(bill=the_bill,flatmate2=john))
+print("John pays", john.pays(bill=the_bill, flatemate2=marry))
+print("Marry pays", marry.pays(bill=the_bill, flatemate2=john))
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatemate1=john, flatemate2= marry, bill=the_bill)
